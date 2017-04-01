78481faea4817b8b7704cd7aab73769f0d74a252
Haimishasha <1417358169@qq.com>
2017-04-01 14:56:04 +0800

    数据库日志接口TBLogMapper经测试没有问题

--- src/main/java/com/webleader/appms/db/mapper/system/TBLogMapper.java
+++ src/main/java/com/webleader/appms/db/mapper/system/TBLogMapper.java
@@ -1,17 +1,91 @@
 package com.webleader.appms.db.mapper.system;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
 import com.webleader.appms.bean.system.TBLog;
 
+/**
+ * @className TBLogMapper
+ * @description 数据库关于TBLog的接口
+ * @author HaoShaSha
+ * @date 2017年4月1日 上午9:46:42
+ * @version 1.0.0
+ */
 public interface TBLogMapper {
-    int deleteByPrimaryKey(String logId);
+	
+	/*****************START BY HaoShaSha*********/
+	
+	/*****************查询接口开始*******************/
+	
+	/** 
+	 * @description 根据日志编号查询日志信息
+	 * @param moduleId 
+	 * @return
+	 * @throws SQLException 
+	 */
+	public TBLog selectByPrimaryKey(String logId) throws SQLException;
+	
+	/** 
+	 * @description 组合条件分页查询日志信息
+	 * @param pageCondition(userId,userName,opType,opDate,pageSize,pageBegin)
+	 * @return
+	 * @throws SQLException 
+	 */
+	public List<TBLog> getLogByPageCondition(Map<Object,Object> pageCondition) throws SQLException;
+	
+	/** 
+	 * @description 统计符合条件的日志数量
+	 * @param pageCondition(userId,userName,opType,opDate,pageSize,pageBegin)
+	 * @return
+	 * @throws SQLException 
+	 */
+	public int getCountByConditon(Map<Object,Object> pageCondition) throws SQLException;
+	
+	/*****************查询接口结束*******************/
+	/*****************插入接口开始*******************/
+	
+	/** 
+	 * @description 添加日志
+	 * @param tbLog
+	 * @return
+	 * @throws SQLException 
+	 */
+	public int insert(TBLog tbLog) throws SQLException;
+	
+	/** 
+	 * @description 选择性的添加日志（同insert）
+	 * @param tbLog
+	 * @return
+	 * @throws SQLException 
+	 */
+	public int insertSelective(TBLog tbLog) throws SQLException;
+	
+	/*****************插入接口结束*******************/
+	/*****************删除接口开始*******************/
+	
+	/** 
+	 * @description 根据日志编号删除日志
+	 * @param moduleId
+	 * @return
+	 * @throws SQLException 
+	 */
+	public int deleteByPrimaryKey(String moduleId) throws SQLException;
+	
 
-    int insert(TBLog record);
+	/*****************删除接口结束*******************/
+	/*****************更新接口开始*******************/
+	
+	/** 
+	 * @description 更新日志信息（应该用不到）
+	 * @param tbLog
+	 * @return
+	 * @throws SQLException 
+	 */
+	public int updateByPrimaryKeySelective(TBLog tbLog) throws SQLException;
+	
+	/*****************更新接口结束*******************/
+	/*****************END BY HaoShaSha***********/
 
-    int insertSelective(TBLog record);
-
-    TBLog selectByPrimaryKey(String logId);
-
-    int updateByPrimaryKeySelective(TBLog record);
-
-    int updateByPrimaryKey(TBLog record);
 }
---
+++ src/test/java/com/webleader/appms/system/TBLogTest.java
@@ -0,0 +1,169 @@
+package com.webleader.appms.system;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.webleader.appms.bean.system.TBLog;
+import com.webleader.appms.db.mapper.system.TBLogMapper;
+
+/**
+ * @className TBLogTest
+ * @description 测试数据库接口 TBLogMapper
+ * @author HaoShaSha
+ * @date 2017年3月31日 下午10:10:40
+ * @version 1.0.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class TBLogTest {
+	
+	@Autowired
+	private TBLogMapper tbLogMapper;
+	
+	
+	/*****************START BY HaoShaSha*********/
+	
+	/*****************查询接口开始*******************/
+	/** 
+	 * @description 根据日志编号查询日志信息
+	 */
+	@Test
+	public void selectByPrimaryKey(){
+		String logId = "a";
+		try {
+			TBLog tbLog = tbLogMapper.selectByPrimaryKey(logId);
+			System.out.println(tbLog);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/** 
+	 * @description 组合条件分页查询日志信息
+	 */
+	@Test
+	public void getLogByPageCondition(){
+		Map<Object,Object> pageCondition = new HashMap<Object,Object>();
+		pageCondition.put("pageBegin", 0);	//必须是bigint
+		pageCondition.put("pageSize", 3);	//必须是bigint
+		try {
+			List<TBLog> tbLogs = tbLogMapper.getLogByPageCondition(pageCondition);
+			System.out.println(tbLogs.size());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/** 
+	 * @description 统计符合条件的日志数量
+	 */
+	@Test
+	public void getCountByConditon(){
+		Map<Object,Object> pageCondition = new HashMap<Object,Object>();
+		pageCondition.put("pageBegin", 0);	//必须是bigint
+		pageCondition.put("pageSize", 3);	//必须是bigint
+		try {
+			int totalCount = tbLogMapper.getCountByConditon(pageCondition);
+			System.out.println(totalCount);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*****************查询接口结束*******************/
+	/*****************插入接口开始*******************/
+	/** 
+	 * @description 添加日志
+	 */
+	@Test
+	public void insert(){
+		String datetime = "2001-02-16 20:38:40";
+		TBLog tbLog = new TBLog();
+		tbLog.setLogId("2");
+		tbLog.setUserId("a");;
+		tbLog.setUserName("hss");
+		tbLog.setOpType("添加");
+		tbLog.setOpDate(Timestamp.valueOf(datetime));
+		tbLog.setOpContent("添加了了一条日志");
+		try {
+			int count = tbLogMapper.insert(tbLog);
+			System.out.println(count);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	} 
+	/** 
+	 * @description 选择性的添加日志（同insert）
+	 */
+	@Test
+	public void insertSelective(){
+		String datetime = "2001-02-16 20:38:40";
+		TBLog tbLog = new TBLog();
+		tbLog.setLogId("3");
+		tbLog.setUserId("a");;
+		tbLog.setUserName("hss");
+		tbLog.setOpType("添加");
+		tbLog.setOpDate(Timestamp.valueOf(datetime));
+		tbLog.setOpContent("添加了了一条日志");
+		try {
+			int count = tbLogMapper.insert(tbLog);
+			System.out.println(count);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*****************插入接口结束*******************/
+	/*****************删除接口开始*******************/
+	/** 
+	 * @description 根据日志编号删除日志
+	 */
+	@Test
+	public void deleteByPrimaryKey(){
+		String logId = "3";
+		try {
+			int count = tbLogMapper.deleteByPrimaryKey(logId);
+			System.out.println(count);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/*****************删除接口结束*******************/
+	/*****************更新接口开始*******************/
+	/** 
+	 * @description 更新日志信息
+	 */
+	@Test
+	public void updateByPrimaryKeySelective(){
+		String datetime = "2001-02-16 20:38:40";
+		TBLog tbLog = new TBLog();
+		tbLog.setLogId("1");
+		tbLog.setUserId("a");;
+		tbLog.setUserName("hss");
+		tbLog.setOpType("删除");
+		tbLog.setOpDate(Timestamp.valueOf(datetime));
+		tbLog.setOpContent("删除了一条日志");
+		try {
+			int count = tbLogMapper.updateByPrimaryKeySelective(tbLog);
+			System.out.println(count);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*****************更新接口结束*******************/
+	/*****************END BY HaoShaSha***********/
+}
