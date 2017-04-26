43911bcae4332370df97e7d2efd31380f2bf4ce7
Haimishasha <1417358169@qq.com>
2017-04-26 21:00:47 +0800

    提交数据库 历史轨迹表 的测试数据

---
+++ sql/sql_2017_04_26/wujiaoxing.sql
@@ -0,0 +1,83 @@
+/* 历史轨迹表录入数据五角星*/
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('13','1','1','1','1','2017-04-15 18:41:14','SRID=2008;POINT(115.34185699402539 32.234506414937435)','1','hss','11');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('14','1','1','1','1','2017-04-15 18:42:14','SRID=2008;POINT(113.43974397468321 32.23450646590436)','1','hss','11');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('15','1','1','1','1','2017-04-15 18:43:14','SRID=2008;POINT(114.97858565466309 31.11647237773544)','1','hss','11');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('16','1','1','1','1','2017-04-15 18:44:14','SRID=2008;POINT(114.39080050286911 32.92548942565918)','1','hss','11');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('17','1','1','1','1','2017-04-15 18:45:14','SRID=2008;POINT(113.80301516742017 31.116472472233312)','1','hss','11');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('18','1','1','1','1','2017-04-15 18:46:14','SRID=2008;POINT(115.34185699402539 32.234506414937435)','1','hss','11');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('19','1','1','1','2','2017-04-15 18:41:14','SRID=2008;POINT(115.34185699402539 31.616472436380924)','2','hss2','12');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('20','1','1','1','2','2017-04-15 18:42:14','SRID=2008;POINT(113.43974397468321 31.616472385414003)','2','hss2','12');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('21','1','1','1','2','2017-04-15 18:43:14','SRID=2008;POINT(114.97858565466309 32.73450647358292)','2','hss2','12');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('22','1','1','1','2','2017-04-15 18:44:14','SRID=2008;POINT(114.39080050286911 30.92548942565918)','2','hss2','12');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('23','1','1','1','2','2017-04-15 18:45:14','SRID=2008;POINT(113.80301516742017 32.73450637908505)','2','hss2','12');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('24','1','1','1','2','2017-04-15 18:46:14','SRID=2008;POINT(115.34185699402539 31.616472436380924)','2','hss2','12');
+
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('25','1','1','1','3','2017-04-15 18:41:14','SRID=2008;POINT(115.34185 32.23450)','3','hss3','13');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('26','1','1','1','3','2017-04-15 18:42:14','SRID=2008;POINT(113.43974 32.23450)','3','hss3','13');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('27','1','1','1','3','2017-04-15 18:43:14','SRID=2008;POINT(114.97858 31.11647)','3','hss3','13');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('28','1','1','1','3','2017-04-15 18:44:14','SRID=2008;POINT(114.39080 32.92548)','3','hss3','13');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('29','1','1','1','3','2017-04-15 18:45:14','SRID=2008;POINT(113.80301 31.11647)','3','hss3','13');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('30','1','1','1','3','2017-04-15 18:46:14','SRID=2008;POINT(115.34185 32.23450)','3','hss3','13');
+
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('31','1','1','1','4','2017-04-15 18:43:14','SRID=2008;POINT(111 30)','4','hss4','14');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('32','1','1','1','4','2017-04-15 18:44:14','SRID=2008;POINT(112 31)','4','hss4','14');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('33','1','1','1','4','2017-04-15 18:45:14','SRID=2008;POINT(113 32)','4','hss4','14');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('34','1','1','1','4','2017-04-15 18:46:14','SRID=2008;POINT(114 33)','4','hss4','14');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('35','1','1','1','4','2017-04-15 18:47:14','SRID=2008;POINT(115 34)','4','hss4','14');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('36','1','1','1','4','2017-04-15 18:48:14','SRID=2008;POINT(116 35)','4','hss4','14');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('37','1','1','1','4','2017-04-15 18:49:14','SRID=2008;POINT(117 36)','4','hss4','14');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('38','1','1','1','4','2017-04-15 18:50:14','SRID=2008;POINT(118 37)','4','hss4','14');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('39','1','1','1','4','2017-04-15 18:51:14','SRID=2008;POINT(119 38)','4','hss4','14');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('40','1','1','1','4','2017-04-15 18:52:14','SRID=2008;POINT(119.5 38.5)','4','hss4','14');
+
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('41','1','1','1','5','2017-04-15 18:43:14','SRID=2008;POINT(120.5311 31.3652)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('42','1','1','1','5','2017-04-15 18:44:15','SRID=2008;POINT(120.5578 31.3390)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('43','1','1','1','5','2017-04-15 18:45:15','SRID=2008;POINT(120.5623 35.3383)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('44','1','1','1','5','2017-04-15 18:46:15','SRID=2008;POINT(120.5923 31.2795)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('45','1','1','1','5','2017-04-15 18:47:15','SRID=2008;POINT(120.5949 35.2685)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('46','1','1','1','5','2017-04-15 18:48:15','SRID=2008;POINT(120.606803 31.2659)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('47','1','1','1','5','2017-04-15 18:49:15','SRID=2008;POINT(120.6066 35.2707)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('48','1','1','1','5','2017-04-15 18:50:15','SRID=2008;POINT(120.6143 31.2711)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('49','1','1','1','5','2017-04-15 18:51:15','SRID=2008;POINT(120.6178 35.2846)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('50','1','1','1','5','2017-04-15 18:52:15','SRID=2008;POINT(120.63531 31.2870)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('51','1','1','1','5','2017-04-15 18:53:15','SRID=2008;POINT(120.6372 35.2837)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('52','1','1','1','5','2017-04-15 18:54:15','SRID=2008;POINT(120.64341 31.2892)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('53','1','1','1','5','2017-04-15 18:55:15','SRID=2008;POINT(120.653707 35.28808)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('54','1','1','1','5','2017-04-15 18:56:15','SRID=2008;POINT(120.6587 31.2859)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('55','1','1','1','5','2017-04-15 18:57:15','SRID=2008;POINT(120.660157 31.288142)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('56','1','1','1','5','2017-04-15 18:58:15','SRID=2008;POINT(120.661709 31.298719)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('57','1','1','1','5','2017-04-15 18:59:15','SRID=2008;POINT(120.655937 31.3194)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('58','1','1','1','5','2017-04-15 17:00:15','SRID=2008;POINT(120.654671 31.32327)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('59','1','1','1','5','2017-04-15 17:01:15','SRID=2008;POINT(120.648623 31.334765)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('60','1','1','1','5','2017-04-15 17:02:15','SRID=2008;POINT(120.6525 31.3399)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('61','1','1','1','5','2017-04-15 17:03:14','SRID=2008;POINT(120.6469 31.3422)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('62','1','1','1','5','2017-04-15 17:04:15','SRID=2008;POINT(120.6545 31.3430)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('63','1','1','1','5','2017-04-15 17:05:15','SRID=2008;POINT(120.6462 31.3426)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('64','1','1','1','5','2017-04-15 17:06:15','SRID=2008;POINT(120.6373 31.3529)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('65','1','1','1','5','2017-04-15 17:07:15','SRID=2008;POINT(120.6226 31.3525)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('66','1','1','1','5','2017-04-15 17:08:15','SRID=2008;POINT(120.6122 31.3627)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('67','1','1','1','5','2017-04-15 17:09:15','SRID=2008;POINT(120.5937 31.3667)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('68','1','1','1','5','2017-04-15 17:10:15','SRID=2008;POINT(120.5869 31.3617)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('69','1','1','1','5','2017-04-15 17:11:15','SRID=2008;POINT(120.5809 31.3627)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('70','1','1','1','5','2017-04-15 17:12:15','SRID=2008;POINT(120.5745 31.3767)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('71','1','1','1','5','2017-04-15 17:13:15','SRID=2008;POINT(120.5599 31.3852)','5','hss5','15');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('72','1','1','1','5','2017-04-15 17:14:15','SRID=2008;POINT(120.5311 31.3652)','5','hss5','15');
+
+
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('73','1','1','1','6','2017-04-15 18:43:14','SRID=2008;POINT(111 30)','6','hss6','16');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('74','1','1','1','6','2017-04-15 18:44:14','SRID=2008;POINT(110 30)','6','hss6','16');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('75','1','1','1','6','2017-04-15 18:45:14','SRID=2008;POINT(111 29)','6','hss6','16');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('76','1','1','1','6','2017-04-15 18:46:14','SRID=2008;POINT(111 31)','6','hss6','16');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('77','1','1','1','6','2017-04-15 18:47:14','SRID=2008;POINT(110 29)','6','hss6','16');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('78','1','1','1','6','2017-04-15 18:48:14','SRID=2008;POINT(111 30)','6','hss6','16');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('79','1','1','1','6','2017-04-15 18:49:14','SRID=2008;POINT(111 33)','6','hss6','16');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('80','1','1','1','6','2017-04-15 18:50:14','SRID=2008;POINT(108 33)','6','hss6','16');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('81','1','1','1','6','2017-04-15 18:51:14','SRID=2008;POINT(108 30)','6','hss6','16');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('82','1','1','1','6','2017-04-15 18:52:14','SRID=2008;POINT(111 30)','6','hss6','16');
+
+
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('83','1','1','1','7','2017-04-15 18:48:14','SRID=2008;POINT(111 30)','7','hss7','17');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('84','1','1','1','7','2017-04-15 18:49:14','SRID=2008;POINT(111 33)','7','hss7','17');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('85','1','1','1','7','2017-04-15 18:50:14','SRID=2008;POINT(108 33)','7','hss7','17');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('86','1','1','1','7','2017-04-15 18:51:14','SRID=2008;POINT(108 30)','7','hss7','17');
+INSERT INTO "public"."t_ls_staff_info_his" VALUES('87','1','1','1','7','2017-04-15 18:52:14','SRID=2008;POINT(111 30)','7','hss7','17');
